8983150da615e7fc17454a0345dc4488eab08447
Zach <zacharyabyrne@gmail.com>
2015-11-02 22:07:09 -0700

    Simple networked version.
    Still doesn't handle node failure.

--- battledot.py
+++ battledot.py
@@ -1,5 +1,12 @@
 import random
-import uuid
+import socket
+from threading import Thread
+from SimpleXMLRPCServer import SimpleXMLRPCServer as RPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler as RPCHandler
+from xmlrpclib import ServerProxy, Error
+
+
+socket.setdefaulttimeout(0.2)
 
 
 BOARD_SIZE = 10
@@ -11,39 +18,51 @@ def random_pair():
 
 
 class BattleNode(object):
-    def __init__(self):
-        self.uuid = uuid.uuid1()
+    def __init__(self, addr):
+        self.addr = addr
         self.position = random_pair()
         self._victim = None
-        self._attacker = None
+        self._victim_addr = None
+        self.server = RPCServer(addr,requestHandler=RPCHandler)
+        self.server_thread = Thread(target=self.server.serve_forever)
+        self.server.register_function(self.defend, 'defend')
+        self.server.register_function(self.insert, 'insert')
+        self.server_thread.start()
 
     def __eq__(self, other):
-        return self.uuid == other.uuid
+        return self.addr == other.addr
 
     def __ne__(self, other):
-        return self.uuid != other.uuid
+        return self.addr != other.addr
+
+    def __del__(self):
+        self.server.shutdown()
+        self.server_thread.join()
 
     @property
     def victim(self):
-        return self._victim
+        return self._victim_addr
 
     @victim.setter
     def victim(self, val):
-        self._victim = val
-        self._victim.attacker = self
+        self._victim_addr = tuple(val)
+        self._victim = ServerProxy('http://{}:{}'.format(*val))
 
-    @property
-    def attacker(self):
-        return self._attacker
-
-    @attacker.setter
-    def attacker(self, val):
-        self._attacker = val
-
-    def attack(self):
-        pos = random_pair()
-        self._victim.defend(pos)
+    def attack(self, pos=None):
+        if pos is None:
+            pos = random_pair()
+        self.victim = self._victim.defend(pos)
 
     def defend(self, pos):
-        if pos == self.position:
-            self._attacker.victim = self._victim
+        if tuple(pos) == self.position:
+            return self._victim_addr
+        return self.addr
+
+    def insert(self, addr):
+        old_victim = self._victim_addr
+        self.victim = addr
+        return old_victim
+
+    def join(self, addr):
+        attacker = ServerProxy('http://{}:{}'.format(*addr))
+        self.victim = attacker.insert(self.addr)
--- testbattledot.py
+++ testbattledot.py
@@ -4,22 +4,29 @@ from battledot import BattleNode
 
 class TestBattleNode(TestCase):
     def test_set_victim(self):
-        a = BattleNode()
-        b = BattleNode()
-        a.victim = b
-        b.victim = a
-        self.assertEqual(a._victim, b)
-        self.assertEqual(b._attacker, a)
-        self.assertEqual(b._victim, a)
-        self.assertEqual(a._attacker, b)
+        a = BattleNode(('localhost', 9900))
+        b = BattleNode(('localhost', 9901))
+        a.victim = b.addr
+        b.victim = a.addr
+        self.assertEqual(a.victim, b.addr)
+        self.assertEqual(b.victim, a.addr)
 
     def test_defend(self):
-        a = BattleNode()
-        b = BattleNode()
-        c = BattleNode()
-        a.victim = b
-        b.victim = c
-        c.victim = a
-        b.defend(b.position)
-        self.assertEqual(a._victim, c)
-        self.assertEqual(c._attacker, a)
+        a = BattleNode(('localhost', 9902))
+        b = BattleNode(('localhost', 9903))
+        c = BattleNode(('localhost', 9904))
+        a.victim = b.addr
+        b.victim = c.addr
+        c.victim = a.addr
+        a.attack(b.position)
+        self.assertEqual(a.victim, c.addr)
+
+    def test_insert(self):
+        a = BattleNode(('localhost', 9905))
+        b = BattleNode(('localhost', 9906))
+        c = BattleNode(('localhost', 9907))
+        a.victim = b.addr
+        b.victim = a.addr
+        c.join(a.addr)
+        self.assertEqual(a.victim, c.addr)
+        self.assertEqual(c.victim, b.addr)
